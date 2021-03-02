8c5773121a06006f07a8f7004811b9baeb0e3cf7
Adrian <adrian_zinko@op.pl>
2021-03-03 00:16:28 +0100

    init project

---
+++ src/main/java/com/kodilla/test/Calculator.java
@@ -0,0 +1,15 @@
+package com.kodilla.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calculator {
+
+
+
+    public  static void main(String[] args) {
+        SpringApplication.run(Calculator.class, args);
+    }
+
+}
